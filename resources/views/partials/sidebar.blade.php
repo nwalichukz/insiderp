cf3b9e3a78ab3e06b919b13db622ebdc7fde43ce
Emeka <mamahemeka84@gmail.com>
2018-03-08 07:54:50 +0100

    search page

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,19 +2,19 @@
 	<div class="inner-box">
 		<h4>Account</h4>
 		<ul class="lest item">
-			<li><a class="active" href="resume.html">Dashboard</a></li>
-			<li><a href="bookmarked.html">Ongoing Job</a></li>
+			<li><a class="active" href="{{ url('dashboard') }}">Dashboard</a></li>
+			<li><a href="#+">Ongoing Job</a></li>
 			<li><a href="{{ url('offers') }}">Job Offers <span class="notinumber">3</span></a></li>
 			<li><a href="{{ url('jobs-completed') }}">Jobs Completed</a></li>
-			<li><a href="{{ url('messages') }}">Messagess <span class="notinumber">10</span></a></li>
+			<li><a href="{{ url('messages') }}">Messages <span class="notinumber">10</span></a></li>
 		</ul>
 		<h4>Job</h4>
 		<ul class="lest item">
 			<li><a href="{{ url('manage-applications') }}">Manage Applications</a></li>
 		</ul>
 		<ul class="lest">
-			<li><a href="change-password.html">Change Password</a></li>
-			<li><a href="index-2.html">Sing Out</a></li>
+			<li><a href="">Change Password</a></li>
+			<li><a href="">Sing Out</a></li>
 		</ul>
 	</div>
 </div>
