3540c951af2af7f912ca9cb633956137f02e0ec2
nwalichukz <chukznwali@ggmail.com>
2018-05-27 01:54:44 +0100

    oh oh

--- resources/views/email/passwordreset.blade.php
+++ resources/views/email/passwordreset.blade.php
@@ -2,7 +2,7 @@
 	        <p>Your password has been reset !!! </p>
 	        <p>Use this password to login and update your password thereafter if you wish.! </p>
 	        
-	       <p> --------------------------</p>
-	       <p> New Password: {{$password}} </p>
-	       <p> -------------------------- </p>
+	       <p>===================================== </p><br/>
+	          New Password: {{$password}}
+	       <p>====================================== </p>
 	        <h4>Bido Team !!!</h4>
