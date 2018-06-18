e858782ac60a7596da7d3638945b0c361bf11148
nwalichukz <chukznwali@ggmail.com>
2018-06-18 16:20:14 +0100

    ya cool

--- resources/views/email/passwordreset.blade.php
+++ resources/views/email/passwordreset.blade.php
@@ -2,9 +2,9 @@
 <head>
 <title>Password reset </title>
 </head>
-<body style="font-size:1.4em; align:center; background:#EEE;">
+<body style="font-size:1em; align:center; background:#EEE;">
 	<center>
-		<div style="width:800px; background:#FFF; border:1px solid #D3D3D3; ">
+		<div style="width:700px; background:#FFF; border:1px solid #D3D3D3; ">
 			<div>
 	</div>
 		<div style="padding: 0 10px 10px 10px;">
@@ -12,7 +12,7 @@
 			Your password has been reset. <br/>
 			Use the password below and this email to login to your account. You can change it thereafter if you wish.
 			</p>
-			<hr/><strong><p style="text-align:justify;">New Password</strong>: {{$password}} </p><hr/>
+			<hr/><p style="text-align:justify;">New Password: {{$password}} </p><hr/>
 			<p>
 			</p>
 			<center><a href="http://www.bido.com.ng/signin" title="click to go back to bido login page"><button style="background:#6A5ACD ;">Go to bido login page</button> </a></center>
