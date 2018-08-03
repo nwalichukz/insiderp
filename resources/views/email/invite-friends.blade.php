85bf6d40747463bbee90a60747f422d78c3c22d8
nwalichukz <chukznwali@ggmail.com>
2018-08-03 18:29:09 +0100

    ok

--- resources/views/email/invite-friends.blade.php
+++ resources/views/email/invite-friends.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <head>
-<title>Signup notification </title>
+<title>Friend Invitation </title>
 </head>
 <body style="font-size:1.1em; align:center; background:#EEE;">
 
 	<br/>
 <center>
-	<div style="width:700px; background:#fff;">
-		<h2 style="background:#0000CD; color:#FFF; height:38px; padding: 0 0 0 10px">You are Invited to Join Bido </h2>
+	<div style="width:600px; background:#fff;">
+		<h2 style="background:#0000CD; color:#FFF; height:38px; padding: 5px 0 0 10px">You are Invited to Join Bido </h2>
 		<div style="padding: 0 10px 10px 10px;">
 		<p style="text-align:justify; font-size:1.2em;">
 			<strong>{{$name}} </strong> invited you to join Bido.  <br/>
-			 Bido is a social service tool that allow users to post and discuss issues 
-			 in the community.
+			 Bido is a social tool that allow users post news, opinions, articles, questions and get involved in discussing 
+			those issues that affect us and our society especially in Nigeria.
 			</p>
 			<center><a href="http://www.bido.com.ng/register" title="click to Join"><button style="background:#0000FF;">JOIN</button> </a></center>
 			<br/>
