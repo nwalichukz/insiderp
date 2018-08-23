147e6022506792e3a83204abed57bc7ece36417c
nwalichukz <chukznwali@ggmail.com>
2018-08-23 11:29:39 +0100

    ok

--- resources/views/email/invite-friends.blade.php
+++ resources/views/email/invite-friends.blade.php
@@ -6,24 +6,26 @@
 <br/>
 <center>
 	<div style="width:600px; background:#fff; ">
-		<h2 style="background:#0000CD; color:#FFF; height:38px; padding: 0 0 0 10px">You are Invited to Join Bido </h2>
+		<h2 style="background:#3299CC; color:#FFF; height:38px; padding: 0 0 0 10px">You are Invited to Join Bido </h2>
 		<div style="padding: 0 10px 10px 10px;">
 		<p style="text-align:justify; font-size:1.2em;">
 			<strong>{{$name}} </strong>invited you to join Bido.  <br/>
 			 Bido is a social tool that allow users post news, opinions, articles, questions and get involved in discussing 
 			those issues that affect us and our society especially in Nigeria.
 			</p>
-			<center><a href="http://www.bido.com.ng/register" title="click to Join"><button style="background:#0000FF; width:100px; height:30px;">JOIN</button> </a></center>
-			<br/>
-			<h3 style="font-style:italic; font-size:1.4em; text-align:left;">Bido Team</h3>
+			<a href="http://www.bido.com.ng/register" title="click to Join">
+				<center>
+				<button style="background:#3299CC; width:100px; height:30px; color:#FFF;">JOIN</button></center> </a>
+	
+			<a style="text-decoration: none;" href="http://www.bido.com.ng" title="Go to Bido">
+			<h3 style="font-style:italic; font-size:1.4em; text-align:right; margin-right:20px; color:#000; font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;">Bido</h3>
+		</a>
 		</div>
-
 	</div>
 	</center>
 	<br/>
-<b	
 </body>
-
 </html>
 	               
 	    
