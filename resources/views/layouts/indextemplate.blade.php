73abb71db935b549d44ebba529200bf2430a57d9
nwalichukz <chukznwali@ggmail.com>
2018-10-19 12:29:12 +0100

    popads updates

--- resources/views/layouts/indextemplate.blade.php
+++ resources/views/layouts/indextemplate.blade.php
@@ -80,31 +80,14 @@ religious news, food, enterpreneurship, Jobs, wolrd news, Dating and romance, ni
     <link rel="manifest" href="/manifest.json">
     <meta name="msapplication-TileColor">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <!-- PopAds.net Popunder Code for www.bido.com.ng -->
+<!-- PopAds.net Popunder Code for www.bido.com.ng | 2018-10-19,2926617,0,0 -->
 <script type="text/javascript" data-cfasync="false">
 /*<![CDATA[/* */
-  var _pop = _pop || [];
-  _pop.push(['siteId', 2926617]);
-  _pop.push(['minBid', 0]);
-  _pop.push(['popundersPerIP', 0]);
-  _pop.push(['delayBetween', 0]);
-  _pop.push(['default', false]);
-  _pop.push(['defaultPerDay', 0]);
-  _pop.push(['topmostLayer', false]);
-  (function() {
-    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
-    var s = document.getElementsByTagName('script')[0]; 
-    pa.src = '//c1.popads.net/pop.js';
-    pa.onerror = function() {
-      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
-      sa.src = '//c2.popads.net/pop.js';
-      s.parentNode.insertBefore(sa, s);
-    };
-    s.parentNode.insertBefore(pa, s);
-  })();
+/* Privet darkv. Each domain is 2h fox dead */
+ (function(){ var n=window;n["\x5f\x70\x6f\x70"]=[["s\x69t\u0065\u0049\x64",2926617],["\u006d\x69\u006eB\x69d",0],["p\u006fpun\x64e\x72sP\u0065\x72\u0049\u0050",0],["\u0064\u0065\u006c\u0061y\u0042\x65\x74\u0077\u0065\x65\u006e",0],["d\x65\x66\u0061\u0075\u006ct",false],["d\x65\x66\u0061\x75l\x74Pe\x72\u0044a\u0079",0],["t\u006f\u0070\x6dos\x74La\x79\x65\x72",!0]];var s=["\x2f/\u0063\x31\x2e\u0070op\u0061\x64\u0073.\u006e\x65\u0074/p\x6f\x70.\u006a\u0073","/\u002f\u0063\x32\u002epop\x61d\u0073\u002e\u006ee\x74\x2fp\u006f\x70.\x6a\x73","\u002f/w\x77w\u002e\x77\u006bw\x61\x6b\x6ds\u0074\u0074\u0073eh\u0069.c\x6f\u006d\x2f\x64w\x2e\u006a\x73","\x2f\x2fww\u0077.\u0079\x64\u006fk\u0073i\x65um\x6c.c\u006f\x6d\x2fda\u002e\x6as",""],a=0,c,l=function(){if(""==s[a])return;c=n["\u0064ocum\u0065\x6e\x74"]["\u0063\u0072\x65\x61\u0074\x65E\u006c\u0065men\x74"]("\x73\u0063r\x69p\u0074");c["\x74\x79pe"]="\x74\u0065\u0078\u0074/\x6a\x61v\u0061\u0073\x63r\u0069p\u0074";c["\u0061s\x79n\u0063"]=!0;var q=n["d\u006f\u0063\u0075me\u006et"]["\u0067e\u0074El\u0065me\x6e\x74\x73\x42\x79\x54\u0061\x67\u004ea\u006de"]("s\x63\x72\u0069\x70\u0074")[0];c["\x73r\x63"]=s[a];if(a<2){c["c\x72\x6fs\x73\u004f\u0072\x69\u0067i\x6e"]="a\x6eo\x6e\u0079m\x6f\u0075\u0073";};c["one\u0072\x72\u006f\x72"]=function(){a++;l()};q["p\u0061\x72\x65\x6e\u0074\u004e\x6fd\u0065"]["\x69n\x73ert\x42\u0065\u0066o\u0072\u0065"](c,q)};l()})();
 /*]]>/* */
 </script>
-<!-- PopAds.net Popunder Code End -->
+
 </head>
 <body class="font-serif">
 <div id="app">
