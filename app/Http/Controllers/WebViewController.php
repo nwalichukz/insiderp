3f716dfe3333afdc8b8d1dc4eea4016295a72222
nwalichukz <chukznwali@ggmail.com>
2018-11-01 09:17:55 +0100

    betting category removed

--- app/Http/Controllers/WebViewController.php
+++ app/Http/Controllers/WebViewController.php
@@ -776,7 +776,7 @@ public function changePassword(Request $request)
   *
   */
    public function deleteSeed(){
-    DB::table('categories')->where(['name' => 'Campus Gist'])->delete();
+    DB::table('categories')->where(['name' => 'Betting'])->delete();
     return redirect('/');
    }
 
