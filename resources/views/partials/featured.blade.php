bfd6937e36310edfbff9e1d37b7cf7b270839531
nwalichukz <chukznwali@ggmail.com>
2018-11-16 13:00:18 +0100

    featured post link rectified

--- resources/views/partials/featured.blade.php
+++ resources/views/partials/featured.blade.php
@@ -3,12 +3,15 @@
         @foreach($featured as $feature)
             <div class="swiper-slide flex flex-no-shrink bg-white border border-solid h-64 w-64">
                 <div>
+                    <a href="{{ url('/post-full-view/'.$feature->id.'/'.str_slug(strtolower($feature->title), '-')) }}"
+                                title="click to read full details">
                     <div>
                         <img src="{{ asset("images/post/".$Helper->postImageFirst($feature->id)->name) }}" alt="The image for the featured post">
                     </div>
                     <div class="px-4 py-4 flex flex-wrap">
                         <span class="text-sm">{{$Helper->get_title(title_case(strtolower($feature->title)), 10)}}</span>
                     </div>
+                </a>
                 </div>
             </div>
         @endforeach
